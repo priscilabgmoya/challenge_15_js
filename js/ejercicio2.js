acd80e946be6099e04f6e574fe0f77e5c077c1ca
priscilaGarciaMoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2023-03-28 18:03:06 -0300

    se agrega ejercicio 2 del challenge de js

---
+++ js/ejercicio2.js
@@ -0,0 +1,17 @@
+/**
+ * 2-   El usuario ingresa nombre y apellido, debemos verificar si los datos ingresados contienen la letra ('a' y 'm'). 
+ * Mostrar x consola la respuesta (si contiene o no)
+ */
+
+let nombre_apellido = prompt("Ingrese Nombre y Apellido Completo");
+
+if(nombre_apellido !== null){
+    let nombre_apellido_minuscula = nombre_apellido.toLocaleLowerCase();
+    if(nombre_apellido_minuscula.includes("a") === true && nombre_apellido_minuscula.includes("m") ){
+        console.log("si contiene");
+    }else{
+        console.log("no contiene");
+    }
+}else{
+    alert('Ingrese un nombre y apellido correcto');
+}
